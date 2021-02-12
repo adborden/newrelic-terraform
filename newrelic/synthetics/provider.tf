d251eb87bd32d23dcc2796a5f10e45fa67005fcc
Aidan Feldman <aidan.feldman@gsa.gov>
2021-02-11 19:27:49 -0500

    upgrade New Relic provider

--- newrelic/synthetics/provider.tf
+++ newrelic/synthetics/provider.tf
@@ -2,8 +2,13 @@ terraform {
   required_version = "~> 0.14.0"
   required_providers {
     newrelic = {
-      version = "~> 1.20.1"
+      version = "~> 2.18.0"
       source  = "newrelic/newrelic"
     }
   }
+}
+
+provider "newrelic" {
+  account_id = "562946"
+  region     = "US"
 }
