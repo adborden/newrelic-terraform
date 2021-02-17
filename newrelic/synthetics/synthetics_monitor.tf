b6acbb432edf28268dc6ceffb82f4aee5ed9a706
Aidan Feldman <aidan.feldman@gsa.gov>
2021-02-17 12:02:41 -0500

    include URLs that aren't included in the spreadsheet

--- newrelic/synthetics/synthetics_monitor.tf
+++ newrelic/synthetics/synthetics_monitor.tf
@@ -4,8 +4,23 @@ data "local_file" "sites" {
 
 locals {
   sites = csvdecode(data.local_file.sites.content)
+
+  # URLs that don't exist in the spreadsheet
+  sites_with_extras = concat(local.sites, [
+    {
+      "Public-Facing Sites/Domains" = "hackerone.com/tts"
+      "Sub- office"                 = "TTS"
+      "Production Status"           = "Production"
+    },
+    {
+      "Public-Facing Sites/Domains" = "dap.digitalgov.gov/Universal-Federated-Analytics-Min.js"
+      "Sub- office"                 = "TTS"
+      "Production Status"           = "Production"
+    }
+  ])
+
   # convert from a list to a map, and filter to the TTS domains
-  tts_sites_by_domain = { for site in local.sites : site["Public-Facing Sites/Domains"] => site if length(regexall("^TTS", site["Sub- office"])) > 0 }
+  tts_sites_by_domain = { for site in local.sites_with_extras : site["Public-Facing Sites/Domains"] => site if length(regexall("^TTS", site["Sub- office"])) > 0 }
 }
 
 resource "newrelic_synthetics_monitor" "uptime" {
